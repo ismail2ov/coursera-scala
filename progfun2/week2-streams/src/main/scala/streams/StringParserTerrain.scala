6cbd80a137096d79e24e18809e241267ff6414e8
Ismail Ismailov <i.a.ismailov@gmail.com>
2016-07-31 14:09:45 +0200

    Functional Program Design in Scala - Solution of the assignment of the second week

--- progfun2/week2-streams/src/main/scala/streams/StringParserTerrain.scala
+++ progfun2/week2-streams/src/main/scala/streams/StringParserTerrain.scala
@@ -19,9 +19,9 @@ import common._
  * - The `-` character denotes parts which are outside the terrain
  * - `o` denotes fields which are part of the terrain
  * - `S` denotes the start position of the block (which is also considered
-     inside the terrain)
+  * inside the terrain)
  * - `T` denotes the final position of the block (which is also considered
-     inside the terrain)
+  * inside the terrain)
  *
  * In this example, the first and last lines could be omitted, and
  * also the columns that consist of `-` characters only.
@@ -52,7 +52,17 @@ trait StringParserTerrain extends GameDef {
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = {
+    (p: Pos) =>
+      p match {
+        case Pos(x, y) =>
+          try {
+            levelVector(x)(y) != '-'
+          } catch {
+            case ex: IndexOutOfBoundsException => false
+          }
+      }
+  }
 
   /**
    * This function should return the position of character `c` in the
@@ -62,7 +72,11 @@ trait StringParserTerrain extends GameDef {
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+    val x = levelVector.indexWhere(row => row.contains(c))
+    val y = levelVector(x).indexOf(c)
+    new Pos(x, y)
+  }
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
