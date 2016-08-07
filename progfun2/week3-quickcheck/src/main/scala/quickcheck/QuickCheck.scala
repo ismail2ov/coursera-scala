e6e6d1c7226902847a231b140311f6400c03a081
Ismail Ismailov <i.a.ismailov@gmail.com>
2016-08-07 22:59:16 +0200

    Functional Program Design in Scala - Solution of the assignment of the third week

--- progfun2/week3-quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ progfun2/week3-quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,12 +9,66 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = for {
+    k <- arbitrary[A]
+    m <- oneOf(const(empty), genHeap)
+  } yield insert(k, m)
+
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
+  }
+
+  lazy val genMap: Gen[Map[Int,Int]] = for {
+    k <- arbitrary[Int]
+    v <- arbitrary[Int]
+    m <- oneOf(const(Map.empty[Int,Int]), genMap)
+  } yield m.updated(k, v)
+
+  property("min1") = forAll { a: Int =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+
+  property("findMin from two elements") = forAll { (x: Int, y: Int) =>
+    findMin(insert(x, insert(y, empty))) == math.min(x, y)
+  }
+
+  property("empty heap") = forAll { a: Int =>
+    isEmpty(deleteMin(insert(a, empty)))
+  }
+
+  property("sorting") = forAll { (h: H) =>
+    def isSorted(h: H): Boolean =
+      if (isEmpty(h)) true
+      else {
+        val currentMin = findMin(h)
+        val smallerHeap = deleteMin(h)
+        isEmpty(smallerHeap) || (currentMin <= findMin(smallerHeap) && isSorted(smallerHeap))
+      }
+    isSorted(h)
+  }
+
+  property("melding of any two heaps") = forAll { (h1: H, h2: H) =>
+    findMin(meld(h1, h2)) == math.min(findMin(h1), findMin(h2))
+  }
+
+  property("arbitrary element and heap") = forAll { (a: Int, h: H) =>
+    findMin(insert(a, h)) == math.min(a, findMin(h))
+  }
+
+  property("meld") = forAll { (h1: H, h2: H) =>
+    def heapEqual(h1: H, h2: H): Boolean =
+      if (isEmpty(h1) && isEmpty(h2)) true
+      else {
+        val m1 = findMin(h1)
+        val m2 = findMin(h2)
+        m1 == m2 && heapEqual(deleteMin(h1), deleteMin(h2))
+      }
+
+    heapEqual(meld(h1, h2), meld(deleteMin(h1), insert(findMin(h1), h2)))
   }
 
 }
