c0dfa2a3dfd6f156ed64e57f4d3d90f44b8df906
Ismail Ismailov <i.a.ismailov@gmail.com>
2016-08-21 13:27:35 +0200

    Functional Program Design in Scala - Solution of the assignment of the fourth week

--- progfun2/week4-calculator/src/main/scala/calculator/Polynomial.scala
+++ progfun2/week4-calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,26 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal {
+      val bValue = b()
+      (bValue * bValue) - 4 * (a() * c())
+    }
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    val negativeB = Signal(-1 * b())
+    val doubleA = Signal(2 * a())
+    val sqrtDelta = Signal(math.sqrt(delta()))
+
+    Signal {
+      if (delta() < 0) Set()
+      else {
+        Set(
+          (negativeB() + sqrtDelta()) / doubleA(),
+          (negativeB() - sqrtDelta()) / doubleA()
+        )
+      }
+    }
   }
 }
